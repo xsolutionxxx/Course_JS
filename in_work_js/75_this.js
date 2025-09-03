0538067239075329b8564e509edf40ca29c54db8
Nazar <nazartalaievych@gmail.com>
2025-09-03 18:18:13 +0300

    practise with classes / class / this

--- in_work_js/75_this.js
+++ in_work_js/75_this.js
@@ -1,1 +1,96 @@
 "use strict";
+
+/* function showThis() {
+  console.log(this);
+}
+
+showThis(); */
+
+// 1) Звичайна функція: this = window, але якщо use strict - undefined.
+
+/* function showThis(a, b) {
+  console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+
+  console.log(sum());
+}
+
+showThis(4, 5); */
+
+/* const obj = {
+  a: 20,
+  b: 14,
+  sum: function () {
+    console.log(this);
+    function shout() {
+      console.log(this);
+    }
+    shout();
+  },
+};
+
+obj.sum(); */
+
+// 2) Контекст у методів об'єкт - є сам об'єкт.
+
+/* function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function () {
+    console.log(`Hello ${this.name}`);
+  };
+}
+
+let nazar = new User("Nazr", 32142); */
+
+// 3) This в конструкторах і класах - це новий екземпляр об'єкта.
+
+function sayName(surname) {
+  console.log(this);
+  console.log(this.name + " " + surname);
+}
+
+const user = {
+  name: "John",
+};
+
+sayName.call(user, "Smith");
+sayName.apply(user, ["Smith"]);
+
+function count(num) {
+  return this * num;
+}
+
+const double = count.bind(2);
+
+console.log(double(3));
+console.log(double(17));
+
+// 4) Ручна прив`язка this: call, apply, bind.
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  this.style.backgroundColor = "red";
+});
+
+const obj1 = {
+  num: 5,
+  sayNumber: function () {
+    const say = () => {
+      console.log(this);
+    };
+
+    say();
+  },
+};
+
+obj1.sayNumber();
+
+const dbl = (a) => a * 2;
+
+console.log(dbl(3));
--- in_work_js/76_class.js
+++ in_work_js/76_class.js
@@ -1,1 +1,35 @@
 "use strict";
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea());
+console.log(long.calcArea());
+
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+  }
+}
+
+const div = new ColoredRectangleWithText(25, 10, "Hello", "red");
+
+div.showMyProps();
+console.log(div.calcArea());
