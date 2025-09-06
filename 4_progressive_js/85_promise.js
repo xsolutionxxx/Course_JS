72a95dd3b4e769acaba2db8b15800f3f8a208f28
Nazar <nazartalaievych@gmail.com>
2025-09-06 23:56:09 +0300

    Promise

--- 4_progressive_js/85_promise.js
+++ 4_progressive_js/85_promise.js
@@ -1,1 +1,57 @@
 "use strict";
+
+/* console.log("Запрос даних...");
+
+const req = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Підготовка даних...");
+
+    const product = {
+      name: "TV",
+      price: 2000,
+    };
+
+    resolve(product);
+  }, 2000);
+});
+
+req
+  .then((product) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        product.status = "order";
+        resolve(product);
+        // reject();
+      }, 2000);
+    });
+  })
+  .then((data) => {
+    data.modify = true;
+    return data;
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(() => {
+    console.error("Помилка");
+  })
+  .finally(() => {
+    console.log("Finally");
+  }); */
+
+const test = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), time);
+  });
+};
+
+/* test(1000).then(() => console.log("1000ms"));
+test(2000).then(() => console.log("2000ms")); */
+
+/* Promise.all([test(1000), test(2000)]).then(() => {
+  console.log("All");
+}); */
+
+Promise.race([test(1000), test(2000)]).then(() => {
+  console.log("All");
+});
