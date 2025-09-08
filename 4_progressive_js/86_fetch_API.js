4105e285cf62597905c5d0d605cc8838f1643b68
Nazar <nazartalaievych@gmail.com>
2025-09-08 19:15:23 +0300

    fetch API

--- 4_progressive_js/86_fetch_API.js
+++ 4_progressive_js/86_fetch_API.js
@@ -1,1 +1,16 @@
 "use strict";
+
+fetch("https://jsonplaceholder.typicode.com/todos/1") // GET
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  // POST
+  method: "POST",
+  body: JSON.stringify({ name: "Alex" }),
+  headers: {
+    "Content-type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
