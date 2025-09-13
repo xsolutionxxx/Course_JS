5d6db69b4507a2c6caf7ff52ce85f8ab61ffcfeb
Nazar <nazartalaievych@gmail.com>
2025-09-13 18:51:25 +0300

    array method and practise with them

--- 4_progressive_js/87_array_enumeration_method.js
+++ 4_progressive_js/87_array_enumeration_method.js
@@ -1,1 +1,49 @@
 "use strict";
+
+// filter
+
+const names = ["Ivan", "Ann", "Ksenia", "Voldemart"];
+const shortNames = names.filter(function (name) {
+  return name.length < 5;
+}); // [ 'Ivan', 'Ann' ]
+
+console.log(shortNames);
+
+// map
+
+const answers = ["IvAN", "AnnA", "HELLo"];
+const result = answers.map((item) => item.toLowerCase()); // [ 'ivan', 'anna', 'hello' ]
+
+console.log(result);
+
+// every/some
+
+const something1 = [4, "123", "asasd"];
+const something2 = [4, 12, 132];
+console.log(something1.some((item) => typeof item === "number")); // true
+console.log(something2.every((item) => typeof item === "number")); // true
+
+// reduce
+
+const numbers = [4, 5, 6, 7, 1, 2, 4];
+const res = numbers.reduce((sum, current) => sum + current, 3); // 29
+
+console.log(res);
+
+const fruits = ["apple", "orange", "banana", "watermello"];
+const bag = fruits.reduce((sum, current) => `${sum}, ${current}`); // apple, orange, banana, watermello
+
+console.log(bag);
+
+const obj = {
+  ivan: "persone",
+  ann: "persone",
+  dog: "animal",
+  cat: "animal",
+};
+
+const newArr = Object.entries(obj)
+  .filter((item) => item[1] === "persone")
+  .map((name) => name[0]);
+
+console.log(newArr); // [ 'ivan', 'ann' ]
---
+++ 4_progressive_js/t-Array_enumeration_method.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// 1
+const films = [
+  {
+    name: "Titanic",
+    rating: 9,
+  },
+  {
+    name: "Die hard 5",
+    rating: 5,
+  },
+  {
+    name: "Matrix",
+    rating: 8,
+  },
+  {
+    name: "Some bad film",
+    rating: 4,
+  },
+];
+
+function showGoodFilms(list) {
+  return list.filter((film) => film.rating >= 8);
+}
+
+function showListOfFilms(list) {
+  return list.map((film) => film.name).join(", ");
+}
+
+function setFilmsIds(list) {
+  return list.map((film, i) => {
+    film.id = ++i;
+    return film;
+  });
+}
+
+console.log(showGoodFilms(films));
+console.log(showListOfFilms(films));
+console.log(setFilmsIds(films));
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(list) {
+  return list.every((film) => film.id);
+}
+
+console.log(checkFilms(tranformedArray));
+
+// 2
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (list) => {
+  return list
+    .filter((num) => num.amount > 0)
+    .reduce((sum, current) => sum + current.amount, 0);
+};
+
+const getTotalIncomeAmount = (list) => {
+  return list.some((num) => num.amount < 0)
+    ? list.reduce((sum, current) => sum + current.amount, 0)
+    : getPositiveIncomeAmount(list);
+};
+
+console.log(getPositiveIncomeAmount(funds));
+console.log(getTotalIncomeAmount(funds));
